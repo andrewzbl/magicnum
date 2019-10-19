dbd5d96447069a406d3a8ff13e7201b6141d7863
zhoubinglong <zblaa313@qq.com>
2019-10-19 16:00:26 +0800

    paas platform init

---
+++ paas-eureka/src/main/java/cn/metathought/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.metathought.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka
+ *
+ * @author zhoubinglong
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
